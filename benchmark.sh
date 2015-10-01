4fbce728add14350ab46ea9b83c8442e7833b26e
Stefan Marr <git@stefan-marr.de>
2015-10-01 23:48:13 +0200

    Added benchmark.sh for build server
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ benchmark.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rebench -d --scheduler=random --without-nice rebench.conf
+
+REV=`git rev-parse HEAD | cut -c1-8`
+TARGET_PATH=~/benchmark-results/savina/$REV
+mkdir -p $TARGET_PATH
+cp savina.data $TARGET_PATH/
