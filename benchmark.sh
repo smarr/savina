466d736837d0890832deec88db1993806d3ea9b2
Stefan Marr <git@stefan-marr.de>
2015-10-02 06:22:37 +0200

    Increase allowed runtime
    - and do not delete obtained data
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rebench -c -d --scheduler=random --without-nice rebench.conf
+rebench -d --scheduler=random --without-nice rebench.conf
 
 REV=`git rev-parse HEAD | cut -c1-8`
 TARGET_PATH=~/benchmark-results/savina/$REV
